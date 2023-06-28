311820013bf14ab3495f6c83160d9772a3a9b51a
Martin del Rosario <delrosariomartin23@gmail.com>
2023-06-28 16:49:54 +0200

    jQuery to React successfully migrated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [firstName, setFirstName] = useState("")
+  const [secondName, setSecondName] = useState("")
+  const [fullName, setFullName] = useState("")
+  
+  const showFullName = () => {
+    if(firstName !== "" && secondName !== ""){
+      setFullName(firstName + " " + secondName)
+      setFirstName("")
+      setSecondName("")
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Form App</h1>
+        
+        <form>
+
+          <label for="first-name">First Name:</label>
+          <input type="text" id="first-name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+          
+          <label for="last-name">Last Name:</label>
+          <input type="text" id="last-name" value={secondName} onChange={(e) => setSecondName(e.target.value)} />
+          
+        <div>
+          <button type="button" id="submit-button" onClick={showFullName}>Submit</button>
+        </div>
+
+        </form>
+        
+        <h2>Full Name: <br /> {fullName}<span id="full-name"></span></h2>
+
     </div>
   );
 }
